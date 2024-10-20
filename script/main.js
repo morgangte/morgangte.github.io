faa63a8df4b648ce92a34356e44ccde64d72e428
morgangte <morgan.gillette@ecole.ensicaen.fr>
2024-10-20 21:21:17 +0200

    Added html, js and css along with projects data

---
+++ script/main.js
@@ -0,0 +1,57 @@
+import data from "../resource/projects.json" with { type: 'json' };
+
+window.addEventListener('load', main);
+
+function main() {
+    const projects = data["projects"];
+    insertProjects(projects);
+}
+
+function insertProjects(projects) {
+    let projectColumns = document.querySelectorAll(".projects-column");
+
+    for (let i = 0; i < projects.length; i++) {
+        insertProject(projects[i], projectColumns[i % projectColumns.length]);
+    }
+}
+
+function insertProject(project, column) {
+    let article = document.createElement("article");
+    article.classList.add("project");
+
+    article.appendChild(projectElement(project["name"], "p", "project-name"));
+    article.appendChild(projectElement(project["type"] + " - " + project["date"], "p", "project-type"));
+    article.appendChild(projectElement(project["description"], "p", "project-description"));
+    if (project["team-size"] > 1) {
+        article.appendChild(projectElement("Team of " + project["team-size"] + " people.", "p", "project-team-size"));
+    }
+    article.appendChild(projectElement(project["language"].join(", "), "p", "project-language"));
+
+    let links = project["links"];
+    for (let i = 0; i < links.length; i++) {
+        article.appendChild(projectLink(links[i]));
+    }
+
+    column.appendChild(article);
+}
+
+function projectElement(text, element, css) {
+    let el = document.createElement(element);
+    el.classList.add(css);
+    el.innerText = text;
+
+    return el;
+}
+
+function projectLink(link) {
+    let p = document.createElement("p");
+    p.classList.add("project-link");
+
+    let el = document.createElement("a");
+    el.setAttribute("href", link["link"]);
+    el.setAttribute("target", "_blank");
+    el.innerText = link["name"];
+
+    p.appendChild(el);
+    return p;
+}
